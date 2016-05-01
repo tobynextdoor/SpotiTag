c163dd2babfe652e4d00ea62827341c308e7db49
Tobias Wermuth <tobynextdoor@gmail.com>
2016-05-01 21:24:53 +0200

    make system tags more safe

--- app/models/song.rb
+++ app/models/song.rb
@@ -11,10 +11,10 @@ class Song < ActiveRecord::Base
       unless rspotify_track.nil?
         song = Song.new(:spotify_id => spotify_id)
         song.save
-        name_tag = Tag.by_name "name:#{rspotify_track.name}"
-        album_tag = Tag.by_name "album:#{rspotify_track.album.name}"
+        name_tag = Tag.name_tag rspotify_track.name
+        album_tag = Tag.album_tag rspotify_track.album.name
         artist_tags = rspotify_track.artists.map do |artist|
-          Tag.by_name "artist:#{artist.name}"
+          Tag.artist_tag artist.name
         end
         (artist_tags << album_tag << name_tag).each do |tag|
           song.tag_it tag, User.system
@@ -29,25 +29,35 @@ class Song < ActiveRecord::Base
     taggings.where(:user => user, :tag => tag).first_or_create
   end
 
-  def tag_by_prefix(prefix)
+  def tags_by_prefix(prefix)
     tags.where("name LIKE :prefix", prefix: "#{prefix}%")
   end
 
   def name
-    tag_by_prefix("name:").first.name.split(":")[1]
+    if tag = tags_by_prefix(Tag.prefix :name).first
+      tag.name.split(":")[1]
+    else
+      ""
+    end
   end
 
   def album
-    tag_by_prefix("album:").first.name.split(":")[1]
+    if tag = tags_by_prefix(Tag.prefix :album).first
+      tag.name.split(":")[1]
+    else
+      ""
+    end
   end
 
-  def artist
-    tag_by_prefix("artist:").map{|a| a.name.split(":")[1]}.join ", "
+  def artists
+    tags_by_prefix(Tag.prefix :artist).map do |tag|
+      tag.name.split(":")[1]
+    end
   end
 
   def tags_counts
-    @tags ||= taggings.group_by{ |g| g.tag.name }.map do |tag_name, tag_taggings|
-      [tag_name, tag_taggings.count]
+    @tags ||= taggings.group_by{ |g| g.tag }.map do |tag, tag_taggings|
+      [tag, tag_taggings.count]
     end.to_h
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,7 +3,42 @@ class Tag < ActiveRecord::Base
   has_many :songs, :through => :taggings
   has_many :users, :through => :taggings
 
+  @@system_prefixes = {
+    :name => "name:",
+    :album => "album:",
+    :artist => "artist:"
+  }
+
   def self.by_name(tag_name)
+    tag_name = "__#{tag_name}" if Tag.is_system_tag_name? tag_name
+    Tag.raw_by_name tag_name
+  end
+
+  def self.raw_by_name(tag_name)
     tag = Tag.where(:name => tag_name).first_or_create
+  end
+
+  def self.is_system_tag_name?(tag_name)
+    @@system_prefixes.values.any? {|prefix| tag_name.start_with?(prefix)}
+  end
+
+  def self.prefix(prefix_key)
+    @@system_prefixes[prefix_key]
+  end
+
+  def self.name_tag(tag_name)
+    Tag.raw_by_name "#{@@system_prefixes[:name]}#{tag_name}"
+  end
+
+  def self.album_tag(tag_name)
+    Tag.raw_by_name "#{@@system_prefixes[:album]}#{tag_name}"
+  end
+
+  def self.artist_tag(tag_name)
+    Tag.raw_by_name "#{@@system_prefixes[:artist]}#{tag_name}"
+  end
+
+  def is_system_tag?
+    Tag.is_system_tag_name? name
   end
 end
